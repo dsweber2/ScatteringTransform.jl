05636f79a2a07ade5911fb459bf3ca3302522fb4
dsweber2 <david.weber2@protonmail.com>
2022-02-16 17:26:15 -0800

    removing unused files and test usings

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,12 @@
 # tests for the various forms of stParallel for the ScatteringTransform
 # exit()
 # using Revise
-using Distributed
-addprocs(min((Sys.CPU_THREADS) - 2 - nprocs(), 2))
-@everywhere using Interpolations, ContinuousWavelets, LinearAlgebra
-@everywhere using FFTW
-@everywhere using ScatteringTransform
-@everywhere using SharedArrays
-using Test
-using Flux, FourierFilterFlux, CUDA, AbstractFFTs, LinearAlgebra, Statistics
+using ScatteringTransform
+using ContinuousWavelets
+using AbstractFFTs, FFTW
+using Test, LinearAlgebra, Statistics
+using Flux, FourierFilterFlux, CUDA
 using Zygote
-CUDA.allowscalar(true)
 
-include("planTests.jl")
-include("cwtTests.jl")
 include("pathTests.jl")
 include("fluxtests.jl")
